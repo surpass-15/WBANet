5024a3d9da7f29d4ecc4120a3e13c64e55f66612
surpass-15 <122538853+surpass-15@users.noreply.github.com>
2025-03-21 00:01:23 +0800

    Create cga.py

---
+++ lib/modules/cga.py
@@ -0,0 +1,54 @@
+import torch
+from torch import nn
+from einops.layers.torch import Rearrange
+from ..wtconv2d import WTConv2d
+
+class SpatialAttention(nn.Module):
+    def __init__(self):
+        super(SpatialAttention, self).__init__()
+        self.sa = nn.Conv2d(2, 1, 7, padding=3, padding_mode='reflect' ,bias=True).to('cuda')
+        # self.conv_dw = WTConv2d(2, 2,7)
+
+    def forward(self, x):
+        x_avg = torch.mean(x, dim=1, keepdim=True)
+        x_max, _ = torch.max(x, dim=1, keepdim=True)
+        x2 = torch.concat([x_avg, x_max], dim=1)
+        # x2 = self.conv_dw(x2)
+        sattn = self.sa(x2)
+        return sattn
+
+
+class ChannelAttention(nn.Module):
+    def __init__(self, dim, reduction = 8):
+        super(ChannelAttention, self).__init__()
+        self.gap = nn.AdaptiveAvgPool2d(1)
+        self.map = nn.AdaptiveMaxPool2d(1)
+        self.ca = nn.Sequential(
+            nn.Conv2d(dim, dim // reduction, 1, padding=0, bias=True),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(dim // reduction, dim, 1, padding=0, bias=True),
+        ).to('cuda')
+
+    def forward(self, x):
+        x_gap = self.gap(x)
+        x_map= self.map(x)
+        cattn = self.ca(x_gap)
+        return cattn
+
+    
+class PixelAttention(nn.Module):
+    def __init__(self, dim):
+        super(PixelAttention, self).__init__()
+        self.pa2 = nn.Conv2d(2 * dim, dim, 7, padding=3, padding_mode='reflect' ,groups=dim, bias=True).to('cuda')
+        self.sigmoid = nn.Sigmoid()
+        # self.conv_dw = WTConv2d(2 * dim, 2 *dim, 7)
+
+    def forward(self, x, pattn1):
+        B, C, H, W = x.shape
+        x = x.unsqueeze(dim=2) # B, C, 1, H, W
+        pattn1 = pattn1.unsqueeze(dim=2) # B, C, 1, H, W
+        x2 = torch.cat([x, pattn1], dim=2) # B, C, 2, H, W
+        x2 = Rearrange('b c t h w -> b (c t) h w')(x2)
+        pattn2 = self.pa2(x2)
+        pattn2 = self.sigmoid(pattn2)
+        return pattn2
